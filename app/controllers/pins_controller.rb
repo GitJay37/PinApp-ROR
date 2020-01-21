ae15edc64ec44809811f6690dab6c770f810d572
Jeison De Jesús Anillo Pérez <Jeisondejesus@hotmail.es>
2020-01-20 23:13:37 -0500

    Eliminar pines y salida de pagina ok

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -18,9 +18,31 @@ class PinsController < ApplicationController
         end
     end
 
+    def show 
+        pin = Pin.find(params[:id])
+    end
+
+    def edit 
+        pin = Pin.find(params[:id])
+    end
+
+    def update 
+        @pin = Pin.find(params[:id])
+        if @pin.update(pin_params)
+            redirect_to pins_path, notice: "actualizado"
+        end
+    end
+
+    def destroy 
+        pin = Pin.find(params[:id])
+        pin.destroy
+
+        redirect_to pins_path, notice: "elimnado"
+    end
+
     private 
         def pin_params
-            params.require(:pin).permit(:title, :image_url)
+            params.require(:pin).permit(:title, :image_url, :id)
         end
         
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "pins#index"
+  get '/pins/new', to: 'pins#new', as: 'new_pin'
+  post '/pins', to: 'pins#create'
+  get '/pins/:id', to: 'pins#show', as: 'pin'
+  get '/pins/:id/edit', to: 'pins#edit', as: 'edit_pin'
+
+  delete '/pins/:id', to: 'pins#destroy'
   resources :pins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
